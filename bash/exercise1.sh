98b40bfa5d77cca9bb517eff5eae9d5b09741fe2
Sanjiban Sengupta <b518041@iiit-bh.ac.in>
2021-03-26 22:10:59 +0530

    Update: BASH Scripts

--- bash/exercise1.sh
+++ bash/exercise1.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-x=$1
-python - <<START
-import os
-print(round(eval('$x'),4))
-START
-
+bc <<< "scale=4; $1" 
--- bash/exercise2.sh
+++ bash/exercise2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-x=$1
-python - <<START
-import pandas as pd
-df=pd.read_csv('$x',delimiter=',',header=None)
-row,col=df.shape
-for i in range(col):
- print(df[i].mean())
-START
-
+awk -F"," '{
+             for (i=1;i<=NF;i++)
+              { sum[i]+=$i;
+                count[i]=count[i]+length($i);
+              }}
+            END{ 
+            for (i=1;i<=NF;i++)
+            { printf sum[i]/count[i]"," }; 
+              printf "\n"
+            }' < $1
